acd67be9ddb1b94dde1e1cca1ab80fe18f26e90c
AnthonyFerron <anthony.ferron74@gmail.com>
2023-11-22 11:39:29 +0100

    Header and footer

--- credits/index.php
+++ credits/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body class="bg-[url('../Icons/Background.png')] bg-cover bg-[#04151F]">
     <?php include('../header/index.html'); ?>
-    <h1 class="text-white text-center">content</h1>
+    <h1 class="text-white text-center text-3xl">☻ content ☻</h1>
     <?php include('../footer/index.html'); ?>
 </body>
 </html>
