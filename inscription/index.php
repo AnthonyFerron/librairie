1deda72e0529b4ecffe7defbda823961f4d907e7
AnthonyFerron <anthony.ferron74@gmail.com>
2023-11-23 14:58:55 +0100

    correction connexion

--- inscription/index.php
+++ inscription/index.php
@@ -24,6 +24,13 @@
     <div class="rounded-[30px] bg-[linear-gradient(132deg,_rgba(241,_138,_0,_0.70)_26.44%,_rgba(181,_181,_181,_0.20)_61.92%)] [box-shadow:0px_4px_24px_-1px_rgba(0,_0,_0,_0.20)] backdrop-filter backdrop-blur-[10px]  flex  flex-col mt-[8%] w-[25%] h-[2%] px-6 py-12 lg:px-8">
         <div class=" flex  flex-col  sm:mx-auto sm:w-full sm:max-w-sm ">
           <img class="mx-auto h-[55%] w-[30%]" src="../Icons/Avatar.png" alt="Your Company">
+          <div>
+            <label for="name" class="block text-sm flex  justify-center font-medium leading-6 text-white">
+            </label>
+            <div class="mt-4 flex justify-center text-center ">
+              <input class="text-center bg-blur rounded-md rounded-[8px] border-[2px] border-[solid] border-[rgba(254,145,0,0.70)] "  id="email" name="email" type="email" autocomplete="email" required placeholder="Nom Prénom" class="sal  block w-[80%] border-0 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400  sm:text-sm sm:leading-6">
+            </div>
+          </div>
         </div>
       
         <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm ">
@@ -53,15 +60,15 @@
             </div>
       
             <div class="mt-2"> 
-              <button type="submit" href="#" class="button flex w-full justify-center px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
+              <button type="submit" class="button flex w-full justify-center px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
               rounded-[50px] bg-[linear-gradient(99deg,_#F18A00_-17.03%,_rgba(217,_217,_217,_0.00)_108.57%)]  hover:translate-y-1
              ">Connexion</button>
             </div>
           </form>
       
           <p class="mt-5 text-center text-sm text-white">
-            Tu n'es pas membre ? 
-            <a href="../inscription/" class="font-semibold leading-6 text-orange text-[rgba(254,145,0,0.70)] hover:text-indigo-500">Inscrit toi en quelques secondes </a>
+            D&eacute;j&agrave; membre ?
+            <a href="../connexion/" class="font-semibold leading-6 text-orange text-[rgba(254,145,0,0.70)] hover:text-indigo-500">Connecte toi directement ici</a>
           </p>
         </div>
       </div>
