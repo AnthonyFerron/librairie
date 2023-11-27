03df5133e54a9b757c1eba8962ce8468ac62db6d
AnthonyFerron <anthony.ferron74@gmail.com>
2023-11-27 15:28:14 +0100

    aa

--- credits/index.php
+++ credits/index.php
@@ -11,15 +11,15 @@
     <div class="flex justify-center items-center mt-[7%]">
         <div class="flex flex-row justify-around items-center w-[1250px] h-[375px] bg-gradient-to-br from-amber-500 rounded-[30px] shadow backdrop-blur ">
             <div class="">
-                <div class="flex justify-center items-center rounded-full bg-gradient-to-br from-white to-[#F18A00] w-[275px] h-[275px]"><img src="../icons/vinz.png"></div>
+                <div class="flex justify-center items-center rounded-full bg-gradient-to-br from-white to-[#F18A00] w-[275px] h-[275px]"><img src="../Icons/vinz.png"></div>
                 <h1 class="text-center text-white text-3xl">Vincent Cotrez</h1>
             </div>
             <div class="">
-                <div class="flex justify-center items-center rounded-full bg-gradient-to-br from-white to-[#F18A00] w-[275px] h-[275px]"><img src="../icons/antho.png"></div>
+                <div class="flex justify-center items-center rounded-full bg-gradient-to-br from-white to-[#F18A00] w-[275px] h-[275px]"><img src="../Icons\antho.png"></div>
                 <h1 class="text-center text-white text-3xl">Anthony Ferron</h1>
             </div>
             <div class="">
-                <div class="flex justify-center items-center rounded-full bg-gradient-to-br from-white to-[#F18A00] w-[275px] h-[275px]"><img src="../icons/haris.png"></div>
+                <div class="flex justify-center items-center rounded-full bg-gradient-to-br from-white to-[#F18A00] w-[275px] h-[275px]"><img src="../Icons/haris.png"></div>
                 <h1 class="text-center text-white text-3xl">Haris Benlamri</h1>
             </div>
 
