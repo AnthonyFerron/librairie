0b8e89c881425a777df2ce807f0f6685a71400c7
AnthonyFerron <anthony.ferron74@gmail.com>
2023-11-29 09:20:45 +0100

    aa

--- index.php
+++ index.php
@@ -8,48 +8,16 @@
 </head>
 <body>
     <!-- header -->
-    <div class="flex w-[100%] h-[100px] mb-5">
-        <div class="w-[100%] h-[100%] flex bg-gradient-to-br from-[#D9D9D980] to-[#D9D9D91A] backdrop-blur-[15px]">
-            <div class="flex w-[25%] h-[100px] justify-around items-center"> 
-                <a href="./"><img class="flex h-[100px] opacity-100" src="./Icons/logo-noir.png"></a>
-            </div>
-            <div class="flex w-[75%] justify-around items-center text-white text-[175%]"> 
-                <a href="./collection/" class="w-[34%] flex items-center justify-center h-[100px] font-['Oswald']">Nos Livres</a>
-                <a href="./contact/" class="w-[33%] flex items-center justify-center h-[100px] font-['Oswald']">Nous Contacter</a>
-                <a href="./connexion/" class="w-[33%] flex items-center justify-center h-[100px] font-['Oswald']">Espace Membres</a>
-            </div>
-        </div>
-    </div>
+
+    <?php include('../../header/index.html'); ?>
+
     <!-- body -->
 
     <h1>bienvenue sur la librairie Nouvelle Plume<h1>
 
     <!-- footer -->
-    <div class="flex w-full h-[200px] mt-10">
-        <div class="w-full h-full flex bg-gradient-to-br from-[#D9D9D980] to-[#D9D9D91A] backdrop-blur-[15px] justify-around items-center">
-            <div class="flex flex-col w-[10%] justify-around items-center text-white">
-                <img class="flex h-[146px] opacity-100 mb-5 ml-10" src="./Icons/plume.png">
-            </div>
-            <div class="flex flex-col w-[20%] justify-around items-center text-white mr-[75px]"> 
-                <p class="font-['Oswald'] text-xl">Centre commercial de la Part Dieu,</p>
-                <p class="mb-5 font-['Oswald'] text-xl">69003 Lyon</p>
-                <p class="mb-3 font-['Oswald'] text-xl">04 50 25 01 28</p>
-                <p class="font-['Oswald'] text-xl"> librairie.nouvelleplume@yahoo.com</p>
-            </div>
-            <div class="flex flex-col w-[35%] h-full justify-around items-center text-white text-[175%]"> 
-                <a href="./collection/" class="w-full flex items-center justify-center font-['Oswald']">Nos Livres</a>
-                <a href="./contact/" class="w-full flex items-center justify-center font-['Oswald']">Nous Contacter</a>
-                <a href="./connexion/" class="w-full flex items-center justify-center font-['Oswald']">Espace Membres</a>
-            </div>
-            <div class="flex flex-col w-[35%] h-full justify-around items-center text-white text-[175%] mr-[200px]"> 
-                <a href="./rgpd/" class="w-full flex items-center justify-center font-['Oswald']">Politique de confidentialit&eacute;</a>
-                <a href="./mentions-legales/" class="w-full flex items-center justify-center font-['Oswald']">Mentions L&eacute;gales</a>
-                <a href="./credits/" class="w-full flex items-center justify-center font-['Oswald']">Cr&eacute;dits</a>
 
-            </div>
-            <a class="absolute w-[75px] bottom-5 right-[200px]" target="_blank" href="https://m.facebook.com/profile.php?id=61552800912971"><img src="./Icons/facebook.png"></a>
-            <a class="absolute w-[75px] bottom-5 right-[75px]" target="_blank" href="https://www.linkedin.com/company/librairie-nouvelle-plume/"><img src="./Icons/linkedin.png"></a>
-        </div>
-    </div>
+    <?php include('../footer/index.html'); ?>
+
 </body>
 </html>
