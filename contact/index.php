1c6313359958059aac231ec8d083b5346b32aad5
Haris <harisben74@gmail.com>
2023-12-04 16:46:12 +0100

    page de contact (haris)^

---
+++ contact/index.php
@@ -0,0 +1,85 @@
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../dist/output.css" rel="stylesheet">
+        <link href="../src/contact.css" rel="stylesheet">
+
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="52cb5553-ca36-435e-ad36-c7d03e766965";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+
+
+        <title>Document</title>
+    </head>
+
+    <body class="bg-[url('../Icons/Background.png')] bg-cover bg-[#04151F]">
+    <?php include('../header/index.html'); ?>
+
+    <div class="grid grid-cols-1 grid-rows-2 gap-4">
+
+    </div>
+    <div class=" mt-[4%] flex mx-auto text-center justify-center rounded-[20px] text-white text-4xl w-[30%]  bg-[linear-gradient(132deg,_rgba(241,_138,_0,_0.70)_26.44%,_rgba(181,_181,_181,_0.20)_61.92%)] [box-shadow:0px_4px_24px_-1px_rgba(0,_0,_0,_0.20)] backdrop-filter backdrop-blur-[10px] " > Nous contacter !</div>
+    <div class="flex   flex-col-6">
+        <!-- Author: FormBold Team -->
+        <!-- Learn More: https://formbold.com -->
+        <div class=" flex  glass rounded-[30px] bg-[linear-gradient(132deg,_rgba(241,_138,_0,_0.70)_26.44%,_rgba(181,_181,_181,_0.20)_61.92%)] [box-shadow:0px_4px_24px_-1px_rgba(0,_0,_0,_0.20)] backdrop-filter backdrop-blur-[10px]  flex  flex-col mt-[4%] w-[25%] h-[2%] px-6  lg:px-8 rounded-[20px] overflow-hidden  border-[solid] border-[rgb(255,_255,_255)] opacity-[50] mx-auto w-full max-w-[550px] p-[4%] sm:p-[4%]">
+            <form action="https://formbold.com/s/oWP5q" method="POST">
+                <div class="mb-5">
+                    <label for="name" class="mb-3 block text-base font-medium   text-white">
+                        Nom Prénoms
+                    </label>
+                    <input type="text" name="name" id="name" placeholder="Rachid l'artiste"
+                           class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-3 block text-base font-medium text-white">
+                        Adresse Mail
+                    </label>
+                    <input type="email" name="email" id="email" placeholder="example@gmail.com"
+                           class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                </div>
+                <div class="mb-5">
+                    <label for="subject" class="mb-3 block text-base font-medium text-white">
+                        Sujet
+                    </label>
+                    <input type="text" name="subject" id="subject" placeholder="Argent/Oseil"
+                           class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                </div>
+                <div class="mb-5">
+                    <label for="message" class="mb-3 block text-base font-medium text-white">
+                        Message
+                    </label>
+                    <textarea rows="4" name="message" id="message" placeholder="Dit moi tout"
+                              class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"></textarea>
+                </div>
+                <div class="text-white mb-5">
+                    <input type="checkbox" name="politiques_confidentialite" id="politiques_confidentialite" required>
+                    <label for="politiques_confidentialite"><a href="/Images/troll.webp">J'accepte les politiques de confidentialité</a></label>
+                </div>
+                <div>
+                    <button class=" flex flex-row hover:shadow-form rounded-md bg-[#d97706] py-2 px-4 text-base font-semibold text-white outline-none">
+                        <img class="flex  " src="../Icons/send.png" width="25px" height="25px" alt="send" >
+                        Envoyer
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        <div class=" mt-[4%] mx-12 my-auto ">
+            <div class=" p-8 rounded-[20px] bg-[linear-gradient(132deg,_rgba(241,_138,_0,_0.70)_26.44%,_rgba(181,_181,_181,_0.20)_61.92%)] [box-shadow:0px_4px_24px_-1px_rgba(0,_0,_0,_0.20)] backdrop-filter backdrop-blur-[10px]   backdrop-filter backdrop-blur-[10px]" >
+                <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.9823079405537!2d4.796174976179779!3d45.77154481287985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea96c16ba51f%3A0x504f5fbc80d6d324!2sInstitut%20G4!5e0!3m2!1sfr!2sfr!4v1701698641113!5m2!1sfr!2sfr" width="600" height="500"   loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+           <div class="flex justify-between mt-[2%]" >
+            <a href="#"><img width="100px" height="100px" src="../Icons/facebook.png" alt="Facebook"></a>
+            <a href="#"><img width="100px" height="100px" src="../Icons/linkedin.png" alt="Facebook"></a>
+           </div>
+        </div>
+
+    </div>
+    </body>
+    <?php include('../footer/index.html'); ?>
+
+    </html>
