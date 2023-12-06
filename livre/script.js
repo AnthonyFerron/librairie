5ab2949d32559f2ee3db971bceef2b38121b0cf4
AnthonyFerron <anthony.ferron74@gmail.com>
2023-12-06 16:52:01 +0100

    aa

--- livre/script.js
+++ livre/script.js
@@ -1,46 +1,43 @@
-// const btns = document.querySelectorAll(".btn");
-// const books =document.querySelectorAll(".books");
-
-// for (let i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener('click', (e)=>{
-//         e.preventDefault();
-//         const filter = e.target.dataset.filter;
-//         console.log(filter)
-//     })
-// }
-
 const btns=[
 {
     id: 1,
-    name: 'All'
+    name: 'All',
+    tag: 'true'
 },
 {
     id: 2,
-    name: 'Classique'
+    name: 'Classique',
+    tag: 'true'
 },
 {
     id: 3,
-    name: 'Bd'
+    name: 'Bd',
+    tag: 'true'
 },
 {
     id: 4,
-    name: 'Manga'
+    name: 'Manga',
+    tag: 'true'
 },
 {
     id: 5,
-    name: 'Biographie'
+    name: 'Biographie',
+    tag: 'true'
 },
 {
     id: 6,
-    name: 'Enfant'
+    name: 'Enfant',
+    tag: 'true'
 },
 {
     id: 7,
-    name: 'Fantastique'
+    name: 'Fantastique',
+    tag: 'true'
 },
 {
     id: 8,
-    name: 'Polar'
+    name: 'Polar',
+    tag: 'true'
 },
 ]
 
@@ -48,10 +45,16 @@ const filters = [...new Set(btns.map((btn)=>
     {return btn}))]
 
 document.getElementById('btns').innerHTML=filters.map((btn)=>{
-    var {name, id} = btn;
-    return(
-        "<button class='flex justify-center items-center bg-gradient-to-b from-amber-500 to-zinc-500 rounded-[30px] text-3xl w-[196px] h-[55px] text-white mr-5' onclick='filterItems("+(id)+`)'>${name}</button>`
-    )
+    var {name, id, tag} = btn;
+    if(id=='1'){
+        return(
+            "<button class='flex justify-center items-center bg-gradient-to-b from-amber-500 to-zinc-500 rounded-[30px] text-3xl w-[196px] h-[55px] text-white mr-5 font-[Oswald]' onclick='document.location.reload("+(tag)+`)'>${name}</button>`
+    )}
+    else{
+        return(
+            "<button class='flex justify-center items-center bg-gradient-to-b from-amber-500 to-zinc-500 rounded-[30px] text-3xl w-[196px] h-[55px] text-white mr-5 font-[Oswald]' onclick='filterItems("+(id)+`)'>${name}</button>`
+        )
+    }
 }).join('');
 
 const product = [
@@ -60,7 +63,7 @@ const product = [
     image: '../Icons/couvertures/shutterisland.jpg',
     title: 'Shutter Island',
     category: 'Polar',
-    grade: '4.8/5',
+    grade: '4.7/5',
     link: './shutter-island/'
 },
 {
@@ -68,7 +71,7 @@ const product = [
     image: '../Icons/couvertures/la-ferme-des-animaux.jpeg',
     title: 'La ferme des animaux',
     category: 'Classique',
-    grade: '4.8/5',
+    grade: '5/5',
     link: '#'
 },
 {
@@ -76,7 +79,7 @@ const product = [
     image: "../Icons/couvertures/l'odysee-d'homere.jpg",
     title: "L'odysse d'Homere",
     category: 'Classique',
-    grade: '4.8/5',
+    grade: '4.9/5',
     link: '#'
 },
 {
@@ -84,7 +87,7 @@ const product = [
     image: '../Icons/couvertures/harrypotteretleprisonnierdaskaban.jpg',
     title: 'Harry potter 3',
     category: 'Fantastique',
-    grade: '4.8/5',
+    grade: '4.3/5',
     link: './harrypotter/'
 },
 {
@@ -92,7 +95,7 @@ const product = [
     image: '../Icons/couvertures/1984.jpg',
     title: '1984',
     category: 'Classique',
-    grade: '4.8/5',
+    grade: '4.9/5',
     link: './1984/'
 },
 {
@@ -100,7 +103,7 @@ const product = [
     image: '../Icons/couvertures/asterixetobelix.jpg',
     title: 'Asterix et obelix',
     category: 'BD',
-    grade: '4.8/5',
+    grade: '4.7/5',
     link: '#'
 },
 {
@@ -108,7 +111,7 @@ const product = [
     image: '../Icons/couvertures/vendredi-ou-la-vie-sauvage.jpg',
     title: 'Vendredi ou la vie sauvage',
     category: 'Classique',
-    grade: '4.8/5',
+    grade: '4.5/5',
     link: './vendredi/'
 },
 
@@ -125,7 +128,7 @@ const product = [
     image: '../Icons/couvertures/Ma-vie-sans-gravite.jpg',
     title: 'Ma vie dans gravite',
     category: 'Biographie',
-    grade: '4.8/5',
+    grade: '4.5/5',
     link: './sansgravite/'
 },
 {
@@ -149,7 +152,7 @@ const product = [
     image: '../Icons/couvertures/hungerGamestome1.jpg',
     title: 'Hunger Games',
     category: 'Fantastique',
-    grade: '4.8/5',
+    grade: '4.7/5',
     link: '#'
 },
 {
@@ -157,7 +160,7 @@ const product = [
     image: '../Icons/couvertures/tom-et-nana.jpg',
     title: 'Tom-Tom et Nana',
     category: 'Enfant',
-    grade: '4.8/5',
+    grade: '4.2/5',
     link: '#'
 },
 {
@@ -165,7 +168,7 @@ const product = [
     image: '../Icons/couvertures/snk-tome-1.jpg',
     title: 'Attaque des titans',
     category: 'Manga',
-    grade: '4.8/5',
+    grade: '4.6/5',
     link: '#'
 },
 ];
@@ -191,28 +194,26 @@ const displayItem = (items) => {
         var{image, title, grade, category, link} = item;
         return(
             `<a href="${link}">
-            <div class="flex flex-col justify-center items-center h-[500px] mr-[100px]">
-            <img class="h-[250px]" src=${image}></img>
+            <div class="flex flex-col justify-center items-center h-[390px] mr-[55px]">
+            <img class="h-[240px]" src=${image}></img>
             <div class="flex justify-center items-center mt-5">
-            <div class="flex  justify-center items-center w-[250px] bg-gradient-to-br from-amber-500 rounded-[30px]">
-            <h1 class="text-center text-2xl text-white  m-2">${title}</h1>
+            <div class="flex  justify-center items-center w-[180px] bg-gradient-to-br from-amber-500 rounded-[30px]">
+            <h1 class="text-center text-xl text-white font-[Roboto] m-0">${title}</h1>
             </div>
             </div>
             <div class="flex justify-start items-start w-full  mt-2 ">
-            <div class="flex w-[70%] bg-gradient-to-br from-amber-500 rounded-[30px] justify-center items-center">
-            <h1 class="text-2xl text-white">${category}</h1>
+            <div class="flex w-[100px] bg-gradient-to-br from-amber-500 rounded-[30px] justify-center items-center">
+            <h1 class="text-xl text-white font-[Roboto]">${category}</h1>
             </div>
             </div>
             <div class="flex flex-row justify-end items-end w-full mt-2">
-            <div class="flex flex-row bg-gradient-to-br rounded-[30px] from-amber-500 w-[85px]">
-            <img src="../Icons/star.png" class="w-[30px]">
-            <h1 class="text-xl text-white ">${grade}</h1>
+            <div class="flex flex-row justify-center items-center bg-gradient-to-br rounded-[30px] from-amber-500 w-[60px] h-[20px]">
+            <img src="../Icons/star.png" class="w-[20px] h-full">
+            <h1 class="text-md text-white font-[Roboto]">${grade}</h1>
             </div>
             </div>
             </div>
             </a>`)
-
-
     }).join('');
 }
 displayItem(categories);
