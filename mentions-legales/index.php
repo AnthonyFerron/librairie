e231fe94744b4dbdacc7ff59b445eea32d4c3980
AnthonyFerron <anthony.ferron74@gmail.com>
2023-12-07 23:43:24 +0100

    aa

--- mentions-legales/index.php
+++ mentions-legales/index.php
@@ -4,53 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="../style.css">
     <title>Mentions L&eacute;gales</title>
     <link rel="icon" href="../Icons/plume.png">
 </head>
 <body class="bg-[#04151F]">
-    <div class="absolute top-0"><img class="w-screen" src="../../Icons/Background.png"></div>
+    <div class="absolute top-0"><img class="w-screen" src="../Icons/Background.png"></div>
 
-    <?php include('../../header/index.html'); ?>
+    <?php include('../header/index.html'); ?>
 
 <div class="flex justify-center">
-    <div class=" text-white text-center break-after-auto justify-center items-center bg-gradient-to-br from-amber-500 from-[-50%] backdrop-blur-[7px] rounded-[30px] w-[50%] mt-8 mx-">
-            <h1 class="font-bold text-4xl font-['Oswald']">
+    <div class=" text-white text-center break-after-auto justify-center items-center bg-gradient-to-br from-amber-500 from-[-50%] backdrop-blur-[7px] rounded-[30px] w-[50%] mt-8 p-16">
+            <h1 class="font-bold text-3xl font-[Oswald]">
                 Mentions l&eacute;gales
             </h1>
-            <h2 class="text-2xl mb-6 font-['Oswald']">
+            <h2 class="text-2xl mb-6 font-[Oswald]">
                 en vigueur au 07/12/2023
             </h2>
 
             <p class="text-xl mb-8 font-[Roboto]">
-                Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l'"Utilisateur", du site nouvelleplume.fr , ci-après le "Site", les présentes mentions légales.
-                La connexion et la navigation sur le Site par l'Utilisateur implique l'acceptation intégrale et sans réserve des présentes mentions légales.
-                Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+                Conform&eacute;ment aux dispositions des Articles 6-III et 19 de la Loi n&deg;2004-575 du 21 juin 2004 pour la Confiance dans l'&eacute;conomie num&eacute;rique, dite L.C.E.N., il est port&eacute; &agrave; la connaissance des utilisateurs et visiteurs, ci-apr&egrave;s l'"Utilisateur", du site nouvelleplume.fr , ci-apr&egrave;s le "Site", les pr&eacute;sentes mentions l&eacute;gales.
+                La connexion et la navigation sur le Site par l'Utilisateur implique l'acceptation int&eacute;grale et sans r&eacute;serve des pr&eacute;sentes mentions l&eacute;gales.
+                Ces derni&egrave;res sont accessibles sur le Site &agrave; la rubrique « Mentions l&eacute;gales ».
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 1 - L'&eacute;diteur</h1>
+            <h1 class="font-bold text-3xl font-[Oswald]">Article 1 - L'&eacute;diteur</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-            L'édition et la direction de la publication du Site est assurée par  Nouvelleplume, sous le domaine ferron.labo-g4.fr, 
-            domicilié 17 Rue Dr Bouchut, 69003 Lyon , dont l'adresse e-mail est agonon@g-4.fr. Ci-après l'"Editeur".
+            L'&eacute;dition et la direction de la publication du Site est assur&eacute;e par  Nouvelleplume, sous le domaine ferron.labo-g4.fr, 
+            domicili&eacute; 17 Rue Dr Bouchut, 69003 Lyon , dont l'adresse e-mail est agonon@g-4.fr. Ci-apr&egrave;s l'"Editeur".
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 2 - L'h&eacute;bergeur</h1>
+            <h1 class="font-bold text-3xl font-[Oswald]">Article 2 - L'h&eacute;bergeur</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-                L'hébergeur du Site est la société cPanel, dont le siège social est situé à Houston, États-Unis
+                L'h&eacute;bergeur du Site est la soci&eacute;t&eacute; cPanel, dont le si&egrave;ge social est situ&eacute; &agrave; Houston, &eacute;tats-Unis
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 3 - Acc&egrave;s au site</h1>
+            <h1 class="font-bold text-3xl font-[Oswald]">Article 3 - Acc&egrave;s au site</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-                Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant 
-                d'une nécessité de maintenance. En cas de modification, interruption ou suspension du Site, l'Éditeur ne saurait être tenu responsable.
+                Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programm&eacute;e ou non et pouvant d&eacute;coulant 
+                d'une n&eacute;cessit&eacute; de maintenance. En cas de modification, interruption ou suspension du Site, l'&eacute;diteur ne saurait &ecirc;tre tenu responsable.
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 4 - Collecte de donn&eacute;es</h1>
+            <h1 class="font-bold text-3xl font-[Oswald]">Article 4 - Collecte de donn&eacute;es</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-                Ce site collecte des données, pour en savoir plus, se référer à la politique de confidentialité
+                Ce site collecte des donn&eacute;es, pour en savoir plus, se r&eacute;f&eacute;rer &agrave; la politique de confidentialit&eacute;
             </p>
     </div>
 </div>
-<?php include('./footer/index.html'); ?>
+<?php include('../footer/index.html'); ?>
 </body>
 </html>
--- rgpd/index.php
+++ rgpd/index.php
@@ -4,67 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="../style.css">
 
     <title>Mentions L&eacute;gales</title>
     <link rel="icon" href="../Icons/plume.png">
 </head>
 <body class="bg-[#04151F]">
-    <div class="absolute top-0"><img class="w-screen" src="../../Icons/Background.png"></div>
+    <div class="absolute top-0"><img class="w-screen" src="../Icons/Background.png"></div>
 
-    <?php include('../../header/index.html'); ?>
+    <?php include('../header/index.html'); ?>
 
 <div class="flex justify-center">
-    <div class=" text-white text-center break-after-auto justify-center items-center bg-gradient-to-br from-amber-500 from-[-50%] backdrop-blur-[7px] rounded-[30px] w-[50%] mt-8 mx-">
-            <h1 class="font-bold text-4xl font-['Oswald']">
-                Politique de Confidentialité - Librairie Nouvelle Plume
+    <div class=" text-white text-center break-after-auto justify-center items-center bg-gradient-to-br from-amber-500 from-[-50%] backdrop-blur-[7px] rounded-[30px] w-[50%] mt-8 p-16">
+            <h1 class="font-bold text-3xl font-['Oswald']">
+                Politique de Confidentialit&eacute; - Librairie Nouvelle Plume
             </h1>
-            <h2 class="text-2xl mb-6 font-['Oswald']">
+            <h2 class="text-xl mb-6 font-['Oswald']">
                 en vigueur au 07/12/2023
             </h2>
 
             <p class="text-xl mb-8 font-[Roboto]">
-                Cette Politique de Confidentialité régit l'utilisation des données personnelles des membres inscrits à la Librairie Nouvelle Plume.
+                Cette Politique de Confidentialit&eacute; r&eacute;git l'utilisation des donn&eacute;es personnelles des membres inscrits &agrave; la Librairie Nouvelle Plume.
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 1 - Collecte des Données Personnelles</h1>
+            <h1 class="font-bold text-3xl font-['Oswald']">Article 1 - Collecte des Donn&eacute;es Personnelles</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-                Nous collectons les données personnelles fournies volontairement par les membres 
-                inscrits, telles que le nom, l'adresse e-mail, l'adresse postale et le numéro de téléphone.
+                Nous collectons les donn&eacute;es personnelles fournies volontairement par les membres 
+                inscrits, telles que le nom, l'adresse e-mail, l'adresse postale et le num&eacute;ro de t&eacute;l&eacute;phone.
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 2 - Utilisation des Données Personnelles</h1>
+            <h1 class="font-bold text-3xl font-['Oswald']">Article 2 - Utilisation des Donn&eacute;es Personnelles</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-                Les données personnelles sont utilisées pour gérer les achats, informer sur 
-                les nouveautés et offres, ainsi que personnaliser l'expérience des membres.
+                Les donn&eacute;es personnelles sont utilis&eacute;es pour g&eacute;rer les achats, informer sur 
+                les nouveaut&eacute;s et offres, ainsi que personnaliser l'exp&eacute;rience des membres.
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 3 - Partage des Données Personnelles</h1>
+            <h1 class="font-bold text-3xl font-['Oswald']">Article 3 - Partage des Donn&eacute;es Personnelles</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-                Les données personnelles ne sont partagées qu'avec des tiers nécessaires 
-                à la prestation de nos services ou en cas d'exigence légale.
+                Les donn&eacute;es personnelles ne sont partag&eacute;es qu'avec des tiers n&eacute;cessaires 
+                &agrave; la prestation de nos services ou en cas d'exigence l&eacute;gale.
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 4 - Sécurité des Données Personnelles</h1>
+            <h1 class="font-bold text-3xl font-['Oswald']">Article 4 - S&eacute;curit&eacute; des Donn&eacute;es Personnelles</h1>
             <p class="text-xl mb-6 font-[Roboto]">
-                Des mesures de sécurité sont en place pour protéger les données 
-                contre tout accès non autorisé, altération ou divulgation.
+                Des mesures de s&eacute;curit&eacute; sont en place pour prot&eacute;ger les donn&eacute;es 
+                contre tout acc&egrave;s non autoris&eacute;, alt&eacute;ration ou divulgation.
             </p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 5 - Droits des Membres</h1>
-            <p class="text-xl mb-6 font-[Roboto]">Les membres ont le droit d'accéder, de rectifier, de supprimer, 
-                de limiter le traitement ou de demander la portabilité de leurs données.</p>
+            <h1 class="font-bold text-3xl font-['Oswald']">Article 5 - Droits des Membres</h1>
+            <p class="text-xl mb-6 font-[Roboto]">Les membres ont le droit d'acc&eacute;der, de rectifier, de supprimer, 
+                de limiter le traitement ou de demander la portabilit&eacute; de leurs donn&eacute;es.</p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 6 - Cookies et Technologies Similaires</h1>
-            <p class="text-xl mb-6 font-[Roboto]">L'utilisation de cookies et de technologies similaires améliore l'expérience en ligne des membres.</p>
+            <h1 class="font-bold text-3xl font-['Oswald']">Article 6 - Cookies et Technologies Similaires</h1>
+            <p class="text-xl mb-6 font-[Roboto]">L'utilisation de cookies et de technologies similaires am&eacute;liore l'exp&eacute;rience en ligne des membres.</p>
 
-            <h1 class="font-bold text-4xl font-['Oswald']">Article 7- Sécurité des Données Personnelles</h1>
-            <p class="text-xl mb-6 mb-12 font-[Roboto]">Nous nous réservons le droit de modifier cette Politique de Confidentialité. 
-                Les membres seront informés des changements.</p>
+            <h1 class="font-bold text-3xl font-['Oswald']">Article 7- S&eacute;curit&eacute; des Donn&eacute;es Personnelles</h1>
+            <p class="text-xl mb-6 font-[Roboto]">Nous nous r&eacute;servons le droit de modifier cette Politique de Confidentialit&eacute;. 
+                Les membres seront inform&eacute;s des changements.</p>
 
-            <p class="font-[Roboto]">Pour toute question, contactez-nous à agonon@g-4.fr.</p>
+            <p class="font-[Roboto]">Pour toute question, contactez-nous &agrave; agonon@g-4.fr.</p>
     </div>
 </div>
-<?php include('./footer/index.html'); ?>
+<?php include('../footer/index.html'); ?>
 </body>
 </html>
