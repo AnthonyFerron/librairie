bf0bfcff41e25971019280797006cb362652635f
AnthonyFerron <anthony.ferron74@gmail.com>
2023-12-07 14:25:43 +0100

    aa

--- livre/1984/joseph.php
+++ livre/1984/joseph.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="../../style.css">
-    <link rel="icon" href="../../Icons/plume.png">
     <title>1984</title>
+    <link rel="icon" href="../../Icons/plume.png">
     <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="52cb5553-ca36-435e-ad36-c7d03e766965";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 </head>
 <body class="bg-[#04151F]">
@@ -18,32 +18,30 @@
     <!-- pres livre -->
 
     <div class="flex justify-center items-center w-full" >
-        <div class="flex flex-col w-[650px] h-[830px] justify-center items-center bg-gradient-to-br from-amber-500 from-[-50%] backdrop-blur-[7px] rounded-[30px]">
+        <div class="flex flex-col w-[650px] h-[875px] justify-center items-center bg-gradient-to-br from-amber-500 from-[-50%] backdrop-blur-[7px] rounded-[30px]">
             <div class="flex justify-around items-center w-[577px] h-[76px] bg-gradient-to-b from-amber-500 rounded-[30px] shadow">
-                <h1 class="text-3xl text-white font-['Oswald']">1984</h1>
+                <h1 class="text-5xl text-white font-['Oswald']">1984</h1>
             </div>
             <img class="flex w-[252px] mt-10 mb-5  opacity-100" src="../../Icons/couvertures/1984.jpg"/>
             <div class="flex flex-row justify-center items-center mb-5 text-white text-xl">
                 <img class="w-[48px]" src="../../Icons/star.png">
                 <p>5/5</p>
             </div>
-            <div class="flex text-white text-center mx-10 text-xl mb-[-30px] font-[Roboto]">
-                <p classs="flex text-white text-center ">En 1984, apr&egrave;s une catastrophe d&eacute;vastatrice, le monde est divis&eacute; 
+            <div class="flex text-white text-center mx-10 text-xl mb-[-50px]">
+                <p classs="flex text-white text-center">En 1984, apr&egrave;s une catastrophe d&eacute;vastatrice, le monde est divis&eacute; 
                     en 3 blocs. En guerre permanente avec Estasia et Eurasia, 
                     Oc&eacute;ania est domin&eacute;e par un gouvernement totalitaire que dirige 
                     l'omnipr&eacute;sent `Big Brother'. Contr&ocirc;l&eacute;e au travers d'innombrables 
                     &eacute;crans, la vie de chacun y est mis&eacute;rable, notamment celles des 
                     prol&eacute;taires, consid&eacute;r&eacute;s comme des sous-hommes. Modeste 
                     employ&eacute; au Miniver, le minist&egrave;re de la V&eacute;rit&eacute;, Winston Smith 
-                    consigne secr&egrave;tement ses r&eacute;flexions dans un journal intime.
-                </p>
-            </div>
+                    consigne secr&egrave;tement ses r&eacute;flexions dans un journal intime.</p>
         </div>
     </div>
 
     <!-- details livres -->
 
-    <div class="flex flex-row w-full justify-center items-center h-[250px] mt-[50px] mb-[100px] font-[Roboto]">
+    <div class="flex flex-row w-full justify-center items-center h-[250px] mt-[50px] mb-[150px]">
         <div class="flex flex-row w-[49%] h-full ml-10">
 
             <div class="flex flex-row justify-around items-center w-[50%] h-full mr-10">
@@ -57,9 +55,9 @@
                 <div class="flex flex-col text-right text-2xl w-[50%] h-full text-white">
                     <p >
                         8€65<br>
-                        <br>Homere <br>
-                        <br>Casterman<br>
-                        <br>2012
+                        <br>George Orwell<br>
+                        <br>Gallimard<br>
+                        <br>Juin 1949
                     </p>
                 </div>
             </div>
@@ -74,31 +72,32 @@
                 </div>
                 <div class="flex flex-col text-right text-2xl w-[50%] h-full text-white">
                     <p>
-                        313<br>
-                        <br>18 x 12,7 x 2,1 cm<br>
-                        <br>0,23 kg<br>
-                        <br>2203033894
+                        376<br>
+                        <br>11x16.90x1.80cm<br>
+                        <br>0.2380 kg<br>
+                        <br>2743620064
                     </p>
                 </div>
             </div>
         </div>
         
         
-        <div alt="barre séparation" class="w-[1px] h-[250px] bg-white"></div>
+        <div alt="barre s&eacute;paration" class="w-[1px] h-[250px] bg-white"></div>
 
         <!-- avis -->
 
 
-        <div class="flex flex-col w-[49%] h-full justify-around items-center ml-10">
+        <div class="flex flex-col w-[49%] h-full justify-around items-center ml-10 mt-6">
 
             <div class="flex justify-items-top flex-col-2 w-full h-[50%]">
                 <div class="flex flex-col-2">
                     <div class="flex">
-                        <img class="flex h-[75px] w-[140px]" src="../../Icons/user.png">
+                        <img class="flex h-[75px] w-[190px]" src="../../Icons/user.png">
                     </div>
                     <div>
-                        <p class="text-justify w-[50%] text-white text-lg font-[Roboto]">L'Odyss&eacute;e d'Hom&egrave;re est une &eacute;pop&eacute;e mythique captivante, m&ecirc;lant les exploits d'Ulysse &agrave; des rencontres mythologiques. 
-                            La po&eacute;sie de l'&eacute;criture enrichit cette lecture incontournable pour les amateurs d'aventures &eacute;piques.</p>
+                        <h2 class="flex text-white font-bold text-xl">User2569</h2>
+                        <p class="text-justify text-white w-[50%] text-lg font-[Roboto]">"1984" de George Orwell plonge les lecteurs dans un monde dystopique oppressant. L'histoire de Winston Smith face &agrave; un r&eacute;gime totalitaire soul&egrave;ve des questions cruciales sur la surveillance et la perte de libert&eacute;. 
+                            Une lecture captivante interrogeant les enjeux de la soci&eacute;t&eacute; et de la libert&eacute; individuelle."</p>
                         <div class="flex flex-row text-white"><img class="w-[25px]" src="../../Icons/star.png"><p>5/5</p></div>
 
                     </div>
@@ -112,19 +111,20 @@
                 
                         <div>
                             <h2 class="flex text-white font-bold text-xl">User2569</h2>
-                            <p class="text-justify text-white text-lg font-[Roboto]">L'Odyss&eacute;e d'Hom&egrave;re, une &eacute;pop&eacute;e captivante m&ecirc;lant exploits d'Ulysse, rencontres mythologiques et po&eacute;sie envoûtante. 
-                                Un incontournable pour les amateurs d'aventures &eacute;piques</p>
+                            <p class="text-justify text-white text-lg font-[Roboto]">"1984" de George Orwell plonge dans une dystopie oppressante. L'histoire de Winston Smith, face &agrave; un r&eacute;gime totalitaire, soul&egrave;ve des questions sur la surveillance et la perte de libert&eacute;. 
+                                Un classique captivant interrogeant les enjeux de la soci&eacute;t&eacute; et de la libert&eacute; individuelle."</p>
                             <div class="flex flex-row text-white items-center"><img class="w-[25px]" src="../../Icons/star.png"><p>5/5</p></div>
                         </div>
                         
-                        <div class="w-[54%] h-[75px]">
-                            <img class="flex w-[100%] h-[75px] ml-1" src="../../Icons/user.png">
+                        <div class="w-[85%] h-[75px]">
+                            <img class="flex h-[75px]" src="../../Icons/user.png">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 
     <?php include('../../footer/index.html'); ?>
 
