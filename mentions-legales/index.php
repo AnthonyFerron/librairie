67696731e910e55de12ff010576db9a6241fccca
ElSombreritoo <145564436+ElSombreritoo@users.noreply.github.com>
2023-12-07 23:32:22 +0100

    ML et RGPPD

---
+++ mentions-legales/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../../style.css">
+    <title>Mentions L&eacute;gales</title>
+    <link rel="icon" href="../Icons/plume.png">
+</head>
+<body class="bg-[#04151F]">
+    <div class="absolute top-0"><img class="w-screen" src="../../Icons/Background.png"></div>
+
+    <?php include('../../header/index.html'); ?>
+
+<div class="flex justify-center">
+    <div class=" text-white text-center break-after-auto justify-center items-center bg-gradient-to-br from-amber-500 from-[-50%] backdrop-blur-[7px] rounded-[30px] w-[50%] mt-8 mx-">
+            <h1 class="font-bold text-4xl font-['Oswald']">
+                Mentions l&eacute;gales
+            </h1>
+            <h2 class="text-2xl mb-6 font-['Oswald']">
+                en vigueur au 07/12/2023
+            </h2>
+
+            <p class="text-xl mb-8 font-[Roboto]">
+                Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l'"Utilisateur", du site nouvelleplume.fr , ci-après le "Site", les présentes mentions légales.
+                La connexion et la navigation sur le Site par l'Utilisateur implique l'acceptation intégrale et sans réserve des présentes mentions légales.
+                Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+            </p>
+
+            <h1 class="font-bold text-4xl font-['Oswald']">Article 1 - L'&eacute;diteur</h1>
+            <p class="text-xl mb-6 font-[Roboto]">
+            L'édition et la direction de la publication du Site est assurée par  Nouvelleplume, sous le domaine ferron.labo-g4.fr, 
+            domicilié 17 Rue Dr Bouchut, 69003 Lyon , dont l'adresse e-mail est agonon@g-4.fr. Ci-après l'"Editeur".
+            </p>
+
+            <h1 class="font-bold text-4xl font-['Oswald']">Article 2 - L'h&eacute;bergeur</h1>
+            <p class="text-xl mb-6 font-[Roboto]">
+                L'hébergeur du Site est la société cPanel, dont le siège social est situé à Houston, États-Unis
+            </p>
+
+            <h1 class="font-bold text-4xl font-['Oswald']">Article 3 - Acc&egrave;s au site</h1>
+            <p class="text-xl mb-6 font-[Roboto]">
+                Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant 
+                d'une nécessité de maintenance. En cas de modification, interruption ou suspension du Site, l'Éditeur ne saurait être tenu responsable.
+            </p>
+
+            <h1 class="font-bold text-4xl font-['Oswald']">Article 4 - Collecte de donn&eacute;es</h1>
+            <p class="text-xl mb-6 font-[Roboto]">
+                Ce site collecte des données, pour en savoir plus, se référer à la politique de confidentialité
+            </p>
+    </div>
+</div>
+<?php include('./footer/index.html'); ?>
+</body>
+</html>
